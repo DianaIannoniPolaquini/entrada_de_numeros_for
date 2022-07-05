143e7a3b74e2efdb6deece38b66a9da0f90987b5
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-04 23:35:28 -0300

    primeiro commit

---
+++ EX_FOR/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EX_FOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada;
+            Console.WriteLine("Quantos números vc deseja que o prog leia? ");
+            entrada = Console.ReadLine();
+            int.TryParse(entrada, out int n);
+
+            int soma = 0;
+            int n1;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Digite o {i}º número: ");
+                n1 = int.Parse(Console.ReadLine());
+                soma += n1;
+            }
+            Console.WriteLine($"A soma dos números inseridos é {soma}");
+        }
+    }
+}
